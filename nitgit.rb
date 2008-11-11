f73459bf21c5bb81d37bf8951ec40c0445d87715
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2008-11-11 01:11:26 -0500

    lookin nicer, tryin to stay organized

--- nitgit.rb
+++ nitgit.rb
@@ -4,55 +4,72 @@ Shoes.setup do
 end
 
 require "grit"
-require "activesupport"
 
-APP_WIDTH = 900
+APP_WIDTH     = 900
 COMMITS_WIDTH = 300
+DIFFS_WIDTH   = APP_WIDTH - COMMITS_WIDTH
+
+# TODO figure out how to override default styles
+BASE_FONT_SIZE = 9
 
 Shoes.app :title => "nitgrit - grit commit browser", :width => APP_WIDTH do
   @repo = Grit::Repo.new File.expand_path("~/diversion/sling/git/SlingIt")
   
   ###
   
-  @commits = stack :width => COMMITS_WIDTH do
-    @repo.commits.each_with_index do |c,i|
-      stack do
-        background i%2==0 ? gray(0.9) : white
+  def view_commit(commit)
+    @diffs.clear { view_diffs commit }
+  end
+  
+  def view_diffs(commit)
+    commit.diffs.each do |d|
+      stack :margin => [0,0,0,10] do
+        border black
         
-        with_options :size => 9, :margin => 0 do |info|
-          info.para c.id
-          info.para c.message
-          info.para c.author
-        end
-        
-        click do
-          @diff.clear do
-            c.diffs.each do |d|
-              stack :margin => [0,0,0,10] do
-                border black
-                
-                d.diff.split("\n").each do |l|
-                  next unless l[0]
-                  stack do
-                    
-                    case l[0].chr
-                    when "+" then background("#0f0")
-                    when "-" then background(red)
-                    else background(gray(0.9))
-                    end
-                    
-                    para l, :size => 9, :margin => 0
-                  end
-                end
+        d.diff.split("\n").each do |l|
+          stack do
+            background(
+              case (l[0] and l[0].chr)
+              when "+" then "#97cb81"
+              when "-" then "#e87770"
+              else gray(0.9)
               end
-            end
+            )
+            
+            para l, :size => BASE_FONT_SIZE, :margin => 0, :family => "Courier"
           end
         end
       end
     end
   end
   
-  @diff = stack :width => APP_WIDTH-COMMITS_WIDTH do
-    
+  ###
+  
+  def stack(options = {}, &block)
+    if padding = options.delete(:padding)
+      super options do
+        background options[:background]
+        super options.merge(:margin => padding), &block
+      end
+    else
+      super
+    end
   end
+  
+  ###
+  
+  @commits = stack :width => COMMITS_WIDTH do
+    @repo.commits.each_with_index do |commit,i|
+      stack :padding => 5, :background => (i%2==0 ? gray(0.9) : white) do
+        info_style = { :size => BASE_FONT_SIZE, :margin => 0 }
+        para commit.id,      info_style
+        para commit.message, info_style
+        para commit.author,  info_style
+        
+        click { view_commit commit }
+      end
+    end
+  end
+  
+  @diffs = stack :width => DIFFS_WIDTH
 end
