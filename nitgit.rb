37a9c12e5caae074f7ce7d5cebb5d2736c0b8d29
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2008-11-11 21:31:29 -0500

    basic support for opening arbitrary repos.. dunno wtf is up with the layout tho

--- nitgit.rb
+++ nitgit.rb
@@ -13,7 +13,7 @@ DIFFS_WIDTH   = APP_WIDTH - COMMITS_WIDTH
 BASE_FONT_SIZE = 9
 
 Shoes.app :title => "nitgrit - grit commit browser", :width => APP_WIDTH do
-  @repo = Grit::Repo.new File.expand_path("~/diversion/sling/git/SlingIt")
+  background "#c3d9ff"
   
   ###
   
@@ -27,19 +27,43 @@ Shoes.app :title => "nitgrit - grit commit browser", :width => APP_WIDTH do
         border black
         
         d.diff.split("\n").each do |l|
-          stack do
-            background(
-              case (l[0] and l[0].chr)
-              when "+" then "#97cb81"
-              when "-" then "#e87770"
-              else gray(0.9)
-              end
-            )
-            
+          stack :padding => 1, :background => background_for_line(l) do
             para l, :size => BASE_FONT_SIZE, :margin => 0, :family => "Courier"
           end
         end
       end
+    end
+  end
+  
+  ###
+  
+  def open_repo(directory)
+    @repo = Grit::Repo.new(directory)
+    load_repo
+  end
+  
+  def load_repo(page = 1)
+    @diffs.clear
+    
+    @commits.clear do
+      @repo.commits.each_with_index do |commit,i|
+        stack :padding => 5, :background => (i%2==0 ? gray(0.9) : white) do
+          info_style = { :size => BASE_FONT_SIZE, :margin => 0 }
+          para commit.id,      info_style
+          para commit.message, info_style
+          para commit.author,  info_style
+
+          click { view_commit commit }
+        end
+      end
+    end
+  end
+  
+  def background_for_line(line)
+    case (line[0] and line[0].chr)
+    when "+" then "#97cb81"
+    when "-" then "#e87770"
+    else gray(0.9)
     end
   end
   
@@ -58,18 +82,15 @@ Shoes.app :title => "nitgrit - grit commit browser", :width => APP_WIDTH do
   
   ###
   
-  @commits = stack :width => COMMITS_WIDTH do
-    @repo.commits.each_with_index do |commit,i|
-      stack :padding => 5, :background => (i%2==0 ? gray(0.9) : white) do
-        info_style = { :size => BASE_FONT_SIZE, :margin => 0 }
-        para commit.id,      info_style
-        para commit.message, info_style
-        para commit.author,  info_style
-        
-        click { view_commit commit }
-      end
+  @menu = flow do
+    background black
+    stroke white
+    
+    button "open repo" do
+      open_repo ask_open_folder
     end
   end
   
-  @diffs = stack :width => DIFFS_WIDTH
+  @commits = stack :width => COMMITS_WIDTH
+  @diffs   = stack :width => 1.0, :margin_left => COMMITS_WIDTH
 end
