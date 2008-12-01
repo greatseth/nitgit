1a47702a91928c62855f1250ba271d76d14fced8
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2008-11-30 20:13:01 -0500

    slow but functional support for highlighting the currently selected commit item

--- lib/commit_list_item.rb
+++ lib/commit_list_item.rb
@@ -2,46 +2,74 @@ require "lib/colors"
 
 class CommitListItem < Shoes::Widget
   include Colors
-    
+  
+  attr_accessor :selected
+  
   def initialize(commit, default_bg_color, options = {})
-    @commit = commit
+    @commit   = commit
+    @selected = false
     
-    stack do
-      background default_bg_color
-      @hover_bg = background(green).hide
-      
+    @stack = stack
+    
+    @default_bg_color = default_bg_color
+    
+    draw_contents
+  end
+  
+  def draw_contents
+    @stack.clear do
+      if @selected
+        background green
+      else
+        background @default_bg_color
+      end
+    
+      @hover_bg = background(green).hide unless @selected
+    
       # size = 36
       # stack :width => size, :margin => [0,0,3,0] do
-      #   image "http://www.gravatar.com/avatar.php?gravatar_id=#{Digest::MD5.hexdigest(commit.author.email.downcase)}&s=#{size}",
+      #   image "http://www.gravatar.com/avatar.php?gravatar_id=#{Digest::MD5.hexdigest(@commit.author.email.downcase)}&s=#{size}",
       #     :width => size, :height => size
       # end
       
-      stack :margin => (@commit.merge? ? 2 : 5) do # :width => -size+4,
+      stack :margin => (@commit.merge? ? 2 : 5) do # , :width => -(size + 5) do
         if @commit.merge?
           para @commit.message, :leading => 1, :size => base_font_size-2,
             :margin => [0,2,0,4], :weight => "bold"
         else
-          para @commit.id, :size => base_font_size, :margin => 0, :stroke => gray(0.6)
-          
+          para @commit.id, :size => base_font_size, :margin => 0,
+            :stroke => (@selected ? black : gray(0.6))
+        
           para @commit.message, :leading => 1, :size => base_font_size,
             :margin => [0,3,0,5]
-          
-          para @commit.author, :size => base_font_size, :margin => 0, :stroke => gray(0.3)
+        
+          para @commit.author, :size => base_font_size, :margin => 0,
+            :stroke => (@selected ? black : gray(0.3))
         end
       end
-      
-      hover { @hover_bg.show }
-      leave { @hover_bg.hide }
-      
+    
+      if not @selected
+        hover { @hover_bg.show }
+        leave { @hover_bg.hide }
+      end
+    
       click do
-        # self changes in the app block, so..
-        commit_item, commit = self, @commit
-        # might just want to change this stuff so the item doesn't know about the app
-        app do
-          @selected_commit = commit_item
-          view_commit commit
+        app do # undo previous selection
+          if formerly_selected = @commits.contents.detect { |ci| ci.selected }
+            formerly_selected.selected = false
+            formerly_selected.draw_contents
+          end
         end
+        
+        @selected = true
+        draw_contents
+        
+        # self changes in the app block, so..
+        commit = @commit
+        
+        app { view_commit commit }
       end
     end
   end
+  
 end
